7c1d5e0049ab491bedaa3a06a240ada58539d40b
James Dungan <jimiwhizz@gmail.com>
2018-11-21 17:30:09 +0000

    initial commit

---
+++ libs/database.py
@@ -0,0 +1,19 @@
+import pymongo
+import os
+
+MONGODB_URI = os.getenv("MONGO_URI")
+# MONGODB_URI = 'mongodb://root:IflsMF01@ds211504.mlab.com:11504/code-institute-data-centric'
+DBS_NAME = "code-institute-data-centric"
+COLLECTION_NAME = "recipeDB"
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected!")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+
+conn = mongo_connect(MONGODB_URI)
+
+coll = conn[DBS_NAME][COLLECTION_NAME]
