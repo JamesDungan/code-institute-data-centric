03bb091b44f956484e57bf6fff29e4cf2128d290
James <jimiwhizz@gmail.com>
2019-03-09 10:20:46 +0000

    WIP uploadFile endpoint

--- libs/image_service.py
+++ libs/image_service.py
@@ -1,7 +1,18 @@
 import boto3
+import os
+from dotenv import load_dotenv
 
-s3 = boto3.resource('s3')
+dotenv_path = 'C:\\Users\\james\\DEV\\CodeInstitute\\code-institute-data-centric\\.env'
+load_dotenv(dotenv_path)
+
+ACCESS_ID = os.environ.get('ACCESS_ID')
+ACCESS_KEY = os.environ.get('ACCESS_KEY')
+
+s3 = boto3.resource('s3',
+    aws_access_key_id=ACCESS_ID,
+    aws_secret_access_key= ACCESS_KEY)
 
 def upload_file(filename, file):
-    s3.Bucket('ci-data-centric-images').put_object(Key=filename, Body=file)
+    response = s3.Bucket('ci-data-centric-images').put_object(Key=filename, Body=file)
+    return response
         
--- rest_server.py
+++ rest_server.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify, render_template, url_for
 from flask_restful import reqparse, abort, Api, Resource, fields, marshal_with
-import os
+import werkzeug, os
 import libs.database as db
+import libs.image_service as iService
 from bson import ObjectId
 from collections import Iterable
 
@@ -48,7 +49,7 @@ class Recipe(Resource):
 class RecipeList(Resource):
     def __init__(self):
         super(RecipeList, self).__init__()
-   
+
     def get(self):
         result = db.find()
         if any('error' in r for r in result):
@@ -71,7 +72,31 @@ class RecipeList(Resource):
                 return result, 500
         return  'database updated', 201
 
+class UploadFile(Resource):
+    def __init__(self):
+        
+        self.reqparser = reqparse.RequestParser()
+        self.reqparser.add_argument('image_file', type=werkzeug.datastructures.FileStorage, location='files')
+        self.baseUrl = 'https://s3-eu-west-1.amazonaws.com/ci-data-centric-images/'
+        super(UploadFile, self).__init__()
+
+    def post(self):
+        print(request.files)
+        data = self.reqparser.parse_args()
+        if data['image_file'] == "":
+            return {
+                'data':'',
+                'message':'No file found',
+                'status':'error'
+                }
+        file = data['image_file']
+        result = iService.upload_file('test', file)
+        return 'test', 200
+
+
 api.add_resource(Recipe, '/reciplease/api/v1.0/recipe/<id>', endpoint='recipe')
 api.add_resource(RecipeList, '/reciplease/api/v1.0/recipes', endpoint='recipes')
+api.add_resource(UploadFile, '/reciplease/api/v1.0/upload', endpoint='upload')
+
 if __name__ == '__main__':
     app.run(debug=True)
